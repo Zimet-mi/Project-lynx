c20c5918acd8490d8fcb131d4f6ea6e840d8955a
Aleksandr Saitgaraev <zimet.mi@gmail.com>
2025-02-07 18:07:37 +0300

    Start of delete dublicate

--- beta/JavaScript.js
+++ beta/JavaScript.js
@@ -130,16 +130,6 @@ document.addEventListener('DOMContentLoaded', async function() {
         document.dispatchEvent(new Event('tableUpdated'));
     };
 
-    const renderData = async () => {
-        try {
-            const data = await fetchDataWithCache();
-            renderTable(data);
-        } catch (error) {
-            console.error(error);
-            alert(`Error: ${error.message}`);
-        }
-    };
-
     await renderData();
     
     // Подключение lightzoom после обновления таблицы
@@ -147,26 +137,3 @@ document.addEventListener('DOMContentLoaded', async function() {
         $('.lightzoom').lightzoom(); // Настройка lightzoom, если используется jQuery
     });
 });
-
-// Функция для переключения вкладок в шапке
- function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-       tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-
