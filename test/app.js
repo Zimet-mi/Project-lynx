82c978ea64af48dfa04828b0bec155c6ee645540
Aleksandr Saitgaraev <zimet.mi@gmail.com>
2025-10-06 11:36:00 +0300

    Try to fix

--- test/app.js
+++ test/app.js
@@ -891,4 +891,5 @@ const App = () => {
 };
 
 // Рендеринг приложения
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
